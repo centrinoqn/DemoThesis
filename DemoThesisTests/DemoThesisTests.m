eacde0172a42a6d02f9b513541e531bd00db903c
walkalone <lamvienbk@gmail.com>
2012-07-16 21:08:23 +0700

    Initial Commit

---
+++ DemoThesisTests/DemoThesisTests.m
@@ -0,0 +1,32 @@
+//
+//  DemoThesisTests.m
+//  DemoThesisTests
+//
+//  Created by VienLam on 7/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DemoThesisTests.h"
+
+@implementation DemoThesisTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DemoThesisTests");
+}
+
+@end
